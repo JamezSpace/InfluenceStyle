a87861acd00b7da765be27ddc9ff880f536eb782
Jamez <adobeclip2003@gmail.com>
2024-08-13 19:30:23 +0100

    Added Responsiveness

--- views/js/index.js
+++ views/js/index.js
@@ -65,3 +65,14 @@ function start_slideshow() {
 addEventListener("load", () => {
     start_slideshow()    
 })
+
+document.getElementById("open-nav-bar").addEventListener("click", e => {
+    if (document.getElementById("mobile-nav-bar").classList.contains("closed")) document.getElementById("mobile-nav-bar").classList.replace("closed", "opened")
+    else document.getElementById("mobile-nav-bar").classList.add("opened")
+})
+
+document.getElementById("close-mobile-nav-bar").addEventListener("click", e => {
+    if (document.getElementById("mobile-nav-bar").classList.contains("opened")) document.getElementById("mobile-nav-bar").style.animationDirection = "reverse"
+
+    document.getElementById("mobile-nav-bar").classList.remove("opened")
+})
