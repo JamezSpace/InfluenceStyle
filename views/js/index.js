30645800a1df95ec2e033a9efda72fc7f2695317
Jamez <adobeclip2003@gmail.com>
2024-08-13 10:56:12 +0100

    New Changes made to Figma Design

--- views/js/index.js
+++ views/js/index.js
@@ -1,26 +1,21 @@
 const infos = [
     {
-        bgImage: "b",
-        h3: 'high jewelry',
-        h1: 'tiffany titan by pharrell williams'
-    },
-    {
-        bgImage: "preloaded-image-2",
+        bgImage: "bb",
         h3: 'fashion',
         h1: 'KHY <br> Fast Fashion Favourites'
     },
     {
-        bgImage: "preloaded-image-3",
+        bgImage: "preloaded-image-2",
         h3: 'high jewelry',
         h1: 'BVLGARI <br> Old Money Luxury'
     },
     {
-        bgImage: "preloaded-image-4",
+        bgImage: "preloaded-image-3",
         h3: 'runway',
         h1: 'Jennie makes her runway debut at Jacquemus show'
     },
     {
-        bgImage: "preloaded-image-5",
+        bgImage: "preloaded-image-4",
         h3: 'destinations',
         h1: 'OSAKA, JAPAN <br> Prepare for your next trip'
     }
